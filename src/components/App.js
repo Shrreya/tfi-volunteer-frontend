cf740839a2599ea482df18d0a50bf052d1567f42
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-29 16:15:09 +0530

    Fetch data & display loading bar on root app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import LoadingBar from 'react-redux-loading';
+import { handleInitialData } from '../actions/shared';
 import UserList from './UserList';
 import CreateForm from './CreateForm';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return (
       <Router>
         <Fragment>
+          <LoadingBar style={{ backgroundColor: '#3CAFCA'}} />
           <Route exact path='/' component={UserList} />
           <Route exact path='/create' component={CreateForm} />
         </Fragment>
@@ -16,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
