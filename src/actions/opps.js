8546196887bdc18e16d942186fa3750931833e24
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-30 18:45:07 +0530

    Added action & dispatches to save new opportunity

--- src/actions/opps.js
+++ src/actions/opps.js
@@ -1,8 +1,16 @@
 export const RECEIVE_OPPS = 'RECEIVE_OPPS';
+export const SAVE_OPP = 'SAVE_OPP';
 
 export function receiveOpps (opps) {
   return {
     type: RECEIVE_OPPS,
     opps
+  }
+}
+
+export function addOpp (opp) {
+  return {
+    type: SAVE_OPP,
+    opp
   }
 }
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,8 @@
-import { getInitialData } from '../utils/api';
+import { getInitialData, saveOpp } from '../utils/api';
 import { receiveCities } from '../actions/cities';
-import { receiveOpps } from '../actions/opps';
+import { receiveOpps, addOpp } from '../actions/opps';
 import { showLoading, hideLoading } from 'react-redux-loading';
+import { push } from 'connected-react-router';
 
 export function handleInitialData () {
   return (dispatch) => {
@@ -11,6 +12,23 @@ export function handleInitialData () {
         ({ cities, opps }) => {
           dispatch(receiveCities(cities));
           dispatch(receiveOpps(opps));
+          dispatch(hideLoading());
+        },
+        (error) => {
+          // TODO: error handling
+        }
+      );
+  }
+}
+
+export function handleSaveOpp (opp) {
+  return (dispatch) => {
+    dispatch(showLoading());
+    return saveOpp(opp)
+      .then(
+        (opp) => {
+          dispatch(addOpp(opp));
+          dispatch(push('/create-success'));
           dispatch(hideLoading());
         },
         (error) => {
