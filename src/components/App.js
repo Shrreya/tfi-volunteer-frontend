86cf4de384a57d88082d5b899391ed3b345ae8cc
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-30 19:10:58 +0530

    Replaced browser router with connected router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import history from '../utils/history';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LoadingBar from 'react-redux-loading';
 import { handleInitialData } from '../actions/shared';
 import UserList from './UserList';
 import CreateForm from './CreateForm';
+import CreateSuccess from './CreateSuccess';
 
 class App extends Component {
 
@@ -14,13 +17,14 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <ConnectedRouter history={history}>
         <Fragment>
           <LoadingBar style={{ backgroundColor: '#3CAFCA'}} />
           <Route exact path='/' component={UserList} />
           <Route exact path='/create' component={CreateForm} />
+          <Route exact path='/create-success' component={CreateSuccess} />
         </Fragment>
-      </Router>
+      </ConnectedRouter>
     );
   }
 }
