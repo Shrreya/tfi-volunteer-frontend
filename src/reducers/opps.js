9bd5618de68083f29d53d00885d327a70a10babe
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-30 18:45:46 +0530

    Added reducer to save new opportunity

--- src/reducers/opps.js
+++ src/reducers/opps.js
@@ -1,9 +1,11 @@
-import { RECEIVE_OPPS } from '../actions/opps';
+import { RECEIVE_OPPS, SAVE_OPP } from '../actions/opps';
 
 export default function opps (state=[], action) {
   switch(action.type) {
     case RECEIVE_OPPS :
       return state.concat(action.opps)
+    case SAVE_OPP :
+      return state.concat(action.opp)
     default :
       return state
   }
